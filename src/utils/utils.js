c85283c967041d94d739a6190e80f0d34d03a039
GrowYourOwnFreedom <bigbelchis@hotmail.com>
2023-05-30 17:21:27 +0100

    reviews complete!

---
+++ src/utils/utils.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const jacksGamesApi = axios.create({
+	baseURL: "https://jack-games.onrender.com/",
+});
+
+export const fetchReviews = (category, sort_by, order) => {
+	let endpoint = `/api/reviews`;
+	// if(category || sort_by ||order) endpoint += `?`
+	if(category) endpoint +=`?category=${category}`
+	if(sort_by) endpoint +=`?sort_by=${sort_by}`
+	if(order) endpoint +=`?order=${order}`
+	return jacksGamesApi.get(endpoint).then(({data:{reviews}}) => {
+		return reviews;
+	});
+};
